5c76447fb3e6a8816094b819c30a52aecf9b9c4a
Andrew Matthews <anma1793@colorado.edu>
2021-11-23 18:25:08 -0700

    Condenced Health API https://github.com/Physics-Comp/Health_Tracker/issues/2

--- Health_API_Notebook/healthAPI.py
+++ Health_API_Notebook/healthAPI.py
@@ -40,51 +40,51 @@ def exerciseID (name):
     return exerciseDict[name]
 
 #Determine the date range (Use excerciseID function to obtain data_type)
-def data_range(name):
+def dataRange(name):
     dates = []
     for record in root.findall('Record'):
         if record.get('type') == exerciseID(name):
             creationDate = record.get('creationDate')
             dates.append(creationDate)
-    print(len(dates))
     print('First Date',dates[0])
     print('Last Date', dates[-1])
 
-#Columns
-columns = []
-
-#Rows
-rows = []
-
 #Show preliminary data about the specified biometric such as the number of data points and date range. 
-rows = []
 def prelimData(name):
-    print('Preliminary',name,'data')
+    print('Preliminary',name,'Data')
+    print('--------------------------------------')
+    
     #Number of child elements in root 
-    numChildren = []
-    for child in root:
-        numChildren.append(child.tag)
-        lengthNumChildren = len(numChildren)
-    print('The number of entries for',name,':', lengthNumChildren)
+    exerciseData = []
+    for record in root.findall('Record'):
+        if record.get('type') == exerciseID(name):
+            value = record.get('value')
+            exerciseData.append(value)
+    lengthOfDataEntries = len(exerciseData)
+    print('The number of entries for',lengthOfDataEntries)
 
     #Display the number of attributes within each element
     numDict = []
     for record in root.iter('Record'):
-        numDict.append(record.attrib)
-        lengthOfDiction = len(numDict)
-    print('The number of dictionaries:',lengthOfDiction)
+        if record.get('type') == exerciseID(name):
+            numDict.append(record.attrib)
+    lengthOfDiction = len(numDict)
+    
+    #List the date range for exercise data
+    dataRange(name)
 
-    #Test Case: The number of child elements in the root should equal the number of dictionaries. If this is not the case then we have missing data entries.
+#Test Case: The number of child elements in the root should equal the number of dictionaries. If this is not the case then we have missing data entries.
     def testElementEqualChild(): 
-        assert lengthNumChildren == lengthOfDiction, "The number of elements do not match the"
+        assert  exerciseData != lengthOfDiction, "Missing data entries (Warning)"
 
-    if __name__ == "__main__":
-        testElementEqualChild()
-        print("Number of elements is equal to the number of dictionaries")
+    testElementEqualChild()
+    print("No missind data entries")
 
 
 #Extract attributes from record element (Heart Rate in beats per min.)
 def exerciseData(start_date,end_date,name):
+    columns = []
+    rows = []
     heartRate = [] #Numerical Heart Rate 
     timeOfEntry = [] #Date of heart rate record
 
@@ -106,17 +106,14 @@ def exerciseData(start_date,end_date,name):
 
     # Create the pandas dataframe
     df = pd.DataFrame(rows,columns = columns)
-    
+   
     #Parse data by dates
-    df.set_index(['Date'])
     df = df.set_index(['Date'])
-    parse_dates = df.loc[start_date:end_date + '1']
-    print(parse_dates)
-    #Writing dataframe to csv
-#     parse_dates.to_csv('health.csv')
-    return parse_dates
-
-# exerciseData('2018-07-19','2018-07-21','HeartRate')
+    exercise_data = df.loc[start_date:end_date]
+    
+    
+    #Return dataframe 
+    return exercise_data
 
 
 
