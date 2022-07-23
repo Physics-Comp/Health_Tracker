592d34ff48183e7f28d0f3ef8415999f4802892a
Andrew Matthews <anma1793@colorado.edu>
2022-07-22 23:22:06 -0500

    Develope the basic structure for calculating VO2 max for men https://github.com/Physics-Comp/Health_Tracker/issues/16

---
+++ Health_API/features/v02_max.py
@@ -0,0 +1,29 @@
+class VO2():
+    """
+    Define typical VO2 max fitness scores for both men and women. VO2 max is the maximum rate of oxygen your body is able to use 
+    during exercise. 
+    """
+    #Constructor
+    def __init__(self):
+        """
+        A list for each fitness level for men containing a tuple value with both age and fitness level ranges for men.
+        """
+        #VO2 calculation level
+        def VO2_calculate(self,fitness_level,age):
+            
+
+class Men(VO2):
+    excellent = [()
+
+    ]
+    """
+    Define the fitness type and age parameters for men
+    """
+
+    #Constructor
+    def __init__(self,fit_level,age):
+        self.fit_level = fit_level
+        self.age = age
+
+    VO2.VO2_calculate(self,fitness_level,age)
+
